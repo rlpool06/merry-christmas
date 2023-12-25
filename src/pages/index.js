d5ddb6e47e953e820fc3bc72bf2cff9e61d7064d
Ryan Pool <rlpool06@gmail.com>
2023-12-25 16:50:12 -0600

    updates to container height and width for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import Image from 'next/image';
 export default function Home() {
 
   return (
-    <div className="flex flex-col justify-center items-center bg-red-500 h-screen">
-      <div className="flex flex-col items-center bg-white w-1/2 p-10 h-1/2 rounded-xl shadow-md">
+    <div className="flex flex-col justify-center items-center bg-red-500 h-screen px-4 md:px-12">
+      <div className="flex flex-col items-center bg-white w-full md:max-w-[600px] p-10 h-auto rounded-xl shadow-md">
         <h1 className="text-2xl font-bold text-green-700">Merry Christmas!</h1>
         <span className="flex flex-wrap w-full justify-center text-center py-4">
           <p className="text-lg">It's a year and half late, but please enjoy this song to celebrate the youngest addition to the Berlanga family.  We love you so much, Alex!</p>
